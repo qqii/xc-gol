617d0a299df73226190a890d199590e6751a305b
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2016-11-16 03:30:52 +0000

    Added Coursework.pdf
    In case safe is ever down

--- game_of_life/src/constants.h
+++ game_of_life/src/constants.h
@@ -1,12 +1,12 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-#define FILENAME_IN "128x128.pgm"
+#define FILENAME_IN "256x256.pgm"
 #define FILENAME_OUT "testout.pgm"
 
 // image height and width
-#define IMHT 128
-#define IMWD 128
+#define IMHT 256
+#define IMWD 256
 
 // number of iterations to run
 #define STEP 10
